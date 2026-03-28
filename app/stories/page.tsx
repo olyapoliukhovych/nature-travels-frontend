c655dfd2bbe12ca0d4aba6f175bf47c5f4470192
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-03-28 18:40:36 +0200

    feat: added storieslist & StoriesCard components. realized basic logic

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,23 +1,12 @@
 import StoriesCategories from "@/components/StoriesCategories/StoriesCategories";
-import StoriesList from "@/components/StoriesList/StoriesList";
-<<<<<<< HEAD
-import { getAllStories } from "@/lib/api/clientApi";
-
-export default async function Stories() {
-  const { stories } = await getAllStories();
-=======
+// import StoriesList from "@/components/StoriesList/StoriesList";
 
 export default function Stories() {
->>>>>>> cfbe94d (Add basic structure to /stories)
   return (
     <div>
       <h1>Статті</h1>
       <StoriesCategories />
-<<<<<<< HEAD
-      <StoriesList stories={stories} />
-=======
-      <StoriesList />
->>>>>>> cfbe94d (Add basic structure to /stories)
+      {/* <StoriesList /> */}
     </div>
   );
 }
