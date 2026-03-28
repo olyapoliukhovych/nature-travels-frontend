33c98e741076ff066f3fe2ef78087cd7555555c6
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-03-28 17:12:56 +0200

    Add basic structure to /stories

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,14 +1,23 @@
 import StoriesCategories from "@/components/StoriesCategories/StoriesCategories";
 import StoriesList from "@/components/StoriesList/StoriesList";
+<<<<<<< HEAD
 import { getAllStories } from "@/lib/api/clientApi";
 
 export default async function Stories() {
   const { stories } = await getAllStories();
+=======
+
+export default function Stories() {
+>>>>>>> cfbe94d (Add basic structure to /stories)
   return (
     <div>
       <h1>Статті</h1>
       <StoriesCategories />
+<<<<<<< HEAD
       <StoriesList stories={stories} />
+=======
+      <StoriesList />
+>>>>>>> cfbe94d (Add basic structure to /stories)
     </div>
   );
 }
