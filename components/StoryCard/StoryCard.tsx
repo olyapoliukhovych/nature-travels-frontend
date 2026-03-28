a42ca55bd7379bad201e8b22147c341f4aa6d0fc
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-03-28 18:40:36 +0200

    feat: added storieslist & StoriesCard components. realized basic logic

---
+++ components/StoryCard/StoryCard.tsx
@@ -0,0 +1,34 @@
+import { Story } from "@/types/types";
+import Image from "next/image";
+import css from "./StoryCard.module.css";
+import { Icon } from "../Icon/Icon";
+import Button from "../Button/Button";
+
+interface Props {
+  story: Story;
+}
+
+export default function StoryCard({ story }: Props) {
+  return (
+    <li className={css.card}>
+      <Image alt={story.title} src={story.img} width={340} height={340} />
+      <div className={css.content}>
+        <div className={css.titleWrapper}>
+          <p>{story.ownerId}</p>
+          <span className={css.point}></span>
+          <Icon id="icon-bookmark" className={css.icon} />
+        </div>
+
+        <h3>{story.title}</h3>
+        <div className={css.buttonWrapper}>
+          <Button variant="neutral" className={css.buttonMore}>
+            Переглянути статтю
+          </Button>
+          <Button variant="neutral" className={css.buttonFav}>
+            <Icon id="icon-bookmark" className={css.buttonIcon} />
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
