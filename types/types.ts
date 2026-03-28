60a8264dc268754a35f31705c499966f9bfc01c8
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-03-28 19:58:05 +0200

    fix: added ... to title in card & more

--- types/types.ts
+++ types/types.ts
@@ -1,22 +1,10 @@
 export interface Story {
   article: string;
-  category: Category;
+  category: string;
   date: string;
   img: string;
   ownerId: string;
   rate: number;
   title: string;
   _id: string;
-}
-
-export interface Category {
-  _id: string;
-  category: string;
-}
-
-export interface Owner {
-  _id: string;
-  name: string;
-  email: string;
-  avatar: string;
 }
