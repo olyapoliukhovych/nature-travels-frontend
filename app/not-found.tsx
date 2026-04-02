69ab7843d10dfc359fee029ca464bf204a426a9b
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-04-02 21:16:37 +0300

    fix(ui): global not-found page with animated redirect & authHeader logo width fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,33 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import css from "@/app/(main)/NotFound.module.css";
+import css from "@/app/NotFound.module.css";
 import AppLink from "@/components/AppLink/AppLink";
 
 export default function NotFound() {
+  const [seconds, setSeconds] = useState(5);
   const router = useRouter();
 
   useEffect(() => {
-    const timer = setTimeout(() => router.push("/"), 5000);
-    return () => clearTimeout(timer);
-  }, [router]);
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    if (seconds === 0) {
+      router.push("/");
+    }
+
+    return () => clearInterval(timer);
+  }, [seconds, router]);
 
   return (
     <div className={css.container}>
       <h2 className={css.title}>404 | Page Not Found</h2>
       <p>Sorry, the page you are looking for does not exist.</p>
       <p className={css.description}>
-        You will be redirected to the home page in 5 seconds.
+        You will be redirected to the home page in
+        <span className={css.timer}> {seconds} </span>seconds...
       </p>
       <AppLink className={css.link} href={"/"} variant={"mantis"}>
         Go back home
