c1d5b071f1e42283d94f56f312ef32f4a922b115
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-04-02 18:48:24 +0300

    feat: create join section + tab/mob

---
+++ components/Join/Join.tsx
@@ -0,0 +1,32 @@
+import AppLink from "../AppLink/AppLink";
+import css from "./Join.module.css";
+
+export default function Join() {
+  return (
+    <section className={css.joinSection}>
+      <div className="container">
+        <div className={css.bgCard}>
+          <div className={css.content}>
+            <h2 className={css.title}>
+              Приєднуйся до спільноти свідомих мандрівників
+            </h2>
+
+            <p className={css.text}>
+              Стань частиною ком’юніті, де подорожі стають не лише пригодою, а й
+              внеском у збереження природи. Тут ти знайдеш однодумців, поради
+              для сталих мандрів та натхнення для нових маршрутів Україною.
+            </p>
+
+            <AppLink
+              href="/auth/register"
+              variant="mantis"
+              className={css.button}
+            >
+              Зареєструватися
+            </AppLink>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
