f01011e4b60234ddf4767b8800991f646548fb35
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-04-03 12:36:06 +0300

    feat: add anchor link

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -3,13 +3,13 @@ import css from "./Join.module.css";
 
 export default function Join() {
   return (
-    <section className={css.joinSection}>
+    <section className={css.joinSection} id="join">
       <div className="container">
         <div className={css.bgCard}>
           <div className={css.content}>
-            <h2 className={css.title}>
+            <h3 className={css.title}>
               Приєднуйся до спільноти свідомих мандрівників
-            </h2>
+            </h3>
 
             <p className={css.text}>
               Стань частиною ком’юніті, де подорожі стають не лише пригодою, а й
