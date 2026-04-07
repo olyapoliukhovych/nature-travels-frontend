7b5198b8ed40a2dcaf9d831e9db9c7a0578389f8
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-04-07 19:35:00 +0300

    fix: update refresh

--- lib/api/auth/clientApi.ts
+++ lib/api/auth/clientApi.ts
@@ -39,23 +39,30 @@ export const logoutUser = async (): Promise<LogoutResponse> => {
   return res.data;
 };
 
+// шлях не той?
+// export const refreshSession = async (): Promise<RefreshSessionResponse> => {
+//   const res = await api.post<RefreshSessionResponse>("/auth/session");
+
+//   return res.data;
+// };
+
+// лишнє?
+// export const checkClientSession = () => {
+//   const cookieString = typeof document !== "undefined" ? document.cookie : "";
+//   const cookiesMap: Record<string, string> = {};
+
+//   cookieString.split(";").forEach((c) => {
+//     const [key, value] = c.split("=").map((s) => s.trim());
+//     if (key && value) cookiesMap[key] = value;
+//   });
+
+//   const hasAccess = Boolean(cookiesMap["accessToken"]);
+//   const hasRefresh = Boolean(cookiesMap["refreshToken"]);
+
+//   return { success: hasAccess && hasRefresh };
+// };
+
 export const refreshSession = async (): Promise<RefreshSessionResponse> => {
-  const res = await api.post<RefreshSessionResponse>("/auth/session");
-
+  const res = await api.post<RefreshSessionResponse>("/auth/refresh");
   return res.data;
-};
-
-export const checkClientSession = () => {
-  const cookieString = typeof document !== "undefined" ? document.cookie : "";
-  const cookiesMap: Record<string, string> = {};
-
-  cookieString.split(";").forEach((c) => {
-    const [key, value] = c.split("=").map((s) => s.trim());
-    if (key && value) cookiesMap[key] = value;
-  });
-
-  const hasAccess = Boolean(cookiesMap["accessToken"]);
-  const hasRefresh = Boolean(cookiesMap["refreshToken"]);
-
-  return { success: hasAccess && hasRefresh };
 };
--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,31 +1,16 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import { User } from "@/types/user";
 
 interface AuthStore {
   user: User | null;
   isAuthenticated: boolean;
-  _hasHydrated: boolean;
   setUser: (user: User) => void;
   clearIsAuthenticated: () => void;
-  setHasHydrated: (state: boolean) => void;
 }
 
-export const useAuthStore = create<AuthStore>()(
-  persist(
-    (set) => ({
-      user: null,
-      isAuthenticated: false,
-      _hasHydrated: false,
-      setUser: (user) => set({ user, isAuthenticated: true }),
-      clearIsAuthenticated: () => set({ user: null, isAuthenticated: false }),
-      setHasHydrated: (state) => set({ _hasHydrated: state }),
-    }),
-    {
-      name: "auth-storage",
-      onRehydrateStorage: () => (state) => {
-        state?.setHasHydrated(true);
-      },
-    },
-  ),
-);
+export const useAuthStore = create<AuthStore>()((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => set({ user, isAuthenticated: true }),
+  clearIsAuthenticated: () => set({ user: null, isAuthenticated: false }),
+}));
