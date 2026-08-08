c5c5f38d2536adfa88437788f8244caf5f60e0d8
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-08-08 23:06:56 +0300

    refactor(profile): migrate profile layout data fetching to client-side auth store state

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,7 +1,5 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import { redirect } from "next/navigation";
-import { getUserProfile } from "@/lib/api/users/serverApi";
 import ProfileLayoutClient from "./ProfileLayoutClient";
 
 export default async function ProfileLayout({
@@ -9,16 +7,10 @@ export default async function ProfileLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getUserProfile();
-
-  if (!user) {
-    redirect("/auth/login");
-  }
-
   return (
     <div className={"layoutWrapper"}>
       <Header />
-      <ProfileLayoutClient user={user}>{children}</ProfileLayoutClient>
+      <ProfileLayoutClient>{children}</ProfileLayoutClient>
       <Footer />
     </div>
   );
